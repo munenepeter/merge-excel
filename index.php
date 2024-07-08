fbe019259c574744eb4257cfe46cb6bd9238a8d9
Peter Munene <peternjega7@gmail.com>
2024-07-08 10:42:59 +0300

    feat: make sure merge command works

--- index.php
+++ index.php
@@ -2,45 +2,63 @@
 
 
 
-use MergeExcel\ConvertCommand;
-use Symfony\Component\Console\Application;
+use MergeExcel\Parser;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 ini_set('memory_limit', '-1'); //# might over use the memory, so remove the limit
 date_default_timezone_set('Africa/Nairobi');
 
 require 'vendor/autoload.php';
 
-// $application = new Application();
-
-
-// $application->add(new \MergeExcel\MergeCommand());
-// $application->add(new ConvertCommand());
-
 if ($argc < 2) {
-    echo "Error: No command provided.\n";
-    echo "Usage:\n";
-    echo "  php index.php convert --workbook=WORKBOOK [--from=FORMAT] [--to=FORMAT]\n";
-    echo "  php index.php merge --path=PATH\n";
+    display_help("Error: No command provided.");
     exit(1);
 }
 
-$commandName = $argv[1];
-if (!in_array($commandName, ['convert', 'merge'])) {
-    echo "Error: Unknown command '$commandName'.\n";
-    echo "Usage:\n";
-    echo "  php index.php convert --workbook=WORKBOOK [--from=FORMAT] [--to=FORMAT]\n";
-    echo "  php index.php merge --path=PATH\n";
+$command_name = $argv[1];
+if (!in_array($command_name, ['convert', 'merge'])) {
+    display_help("Error: Unknown command '$command_name'.");
     exit(1);
 }
 
-// Remove the command name from arguments
+// remove the command name from arguments
 array_shift($argv);
 
 
-switch ($argv) {
+switch ($argv[0]) {
     case 'merge':
-        # code...
+        if (!isset($argv[1]) ||  $argv[1] === '') {
+            display_help_for_merge("Error: Missing path to workbooks to merge.");
+            exit(1);
+        }
+
+        $path = null;
+
+        if (strpos($argv[1], '--path=') !== 0) {
+            display_help_for_merge("Error: Invalid argument format. Expected '--path=uploads");
+            exit(1);            
+        } 
+
+        $path = substr($argv[1], strlen('--path='));
+
+        $parser = new Parser();
+        $output = new ConsoleOutput();
+
+        $workbook_count = count(array_diff(scandir($path), ['..', '.']));
+
+        $progressBar = new ProgressBar($output, $workbook_count);
+        $progressBar->setFormat('<info>%current%/%max% [%bar%] %percent:3s%%</info>');
+        $progressBar->start();
+
+        $parser->merge($path, function ($progress) use ($progressBar) {
+            $progressBar->setProgress($progress);
+        });
+
+        $progressBar->finish();
+        $output->writeln("\nMerge completed!");
         break;
+
     case 'convert':
         # code...
         break;
